71c9de76685bb4d3d7b0c8d8fed59d3221e0395a
Vincent Tern <Vincenttern@gmail.com>
2019-12-18 13:25:31 -0800

    Add truncate and styles for notes

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import numeral from "numeral";
+import Truncate from "react-truncate";
 
-export const ExpenseListItem = ({ description, amount, createdAt, id }) => {
+export const ExpenseListItem = ({
+  description,
+  note,
+  amount,
+  createdAt,
+  id
+}) => {
   return (
     <Link className="list-item" to={`/edit/${id}`}>
-      <div>
+      <div className="list-item__info">
         <h3 className="list-item__title">{description}</h3>
+
+        <div>
+          <Truncate className="list-item__note" lines={1} ellipsis="...">
+            {note}
+          </Truncate>
+        </div>
 
         <span className="list-item__sub-title">
           {moment(createdAt).format("MMMM Do, YYYY")}
